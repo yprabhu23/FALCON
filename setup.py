a273be3a977e7df2610ab0b71a4250c0e55b0d7c
hang0610 <yuanhang0610@gmail.com>
2025-05-18 00:34:04 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='asap',
+    name='falcon',
     version='0.0.1',
-    license="BSD-3-Clause",
+    license="MIT",
     packages=find_packages(),
-    description='ASAP: ligning Simulation and Real-World Physics for Learning Agile Humanoid Whole-Body Skills',
-    url="https://github.com/LeCAR-Lab/ASAP",  # Update this with your actual repository URL
+    description='FALCON: Learning Force-Adaptive Humanoid Loco-Manipulation',
+    url="https://github.com/LeCAR-Lab/FALCON",  # Update this with your actual repository URL
     python_requires=">=3.8",
     install_requires=[
         "hydra-core>=1.2.0",
